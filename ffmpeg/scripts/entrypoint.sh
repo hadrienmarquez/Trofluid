d84ff3096f1372f6379875e14ce42b37abba0413
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-04 16:11:05 +0100

    chmod +x

--- ffmpeg/scripts/entrypoint.sh
+++ ffmpeg/scripts/entrypoint.sh
@@ -3,15 +3,15 @@
 cd /usr/local
 
 # Adding ssh to service list on 'default' runlevel
-rc-update add sshd default
+#rc-update add sshd default
 
 # As we did not boot with openrc, necessary steps to launch sshd service
-rc-status
-mkdir -p /run/openrc
-touch /run/openrc/softlevel
+#rc-status
+#mkdir -p /run/openrc
+#touch /run/openrc/softlevel
 
 # Start the ssh server
-/etc/init.d/sshd start
+/etc/init.d/ssh start
 
 # Copy ssh public key from other container
 mkdir /home/ffmpeg/.ssh
--- ffmpeg/scripts/startStream.sh
+++ ffmpeg/scripts/startStream.sh
@@ -6,4 +6,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/lib64
 export PATH=$PATH:/usr/local/bin
 
 # Here goes the big command
-ffmpeg
+ffmpeg -sources decklink
