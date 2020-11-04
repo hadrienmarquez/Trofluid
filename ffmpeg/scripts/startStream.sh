edc19fafd743b7992d7fcd8fc4b821b98a4ff45c
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-04 19:03:49 +0100

    Adding SRT command to start script

--- ffmpeg/scripts/startStream.sh
+++ ffmpeg/scripts/startStream.sh
@@ -7,3 +7,7 @@ export PATH=$PATH:/usr/local/bin
 
 # Here goes the big command
 ffmpeg -sources decklink
+until ffmpeg -format_code Hp25 -f decklink -i 'DeckLink Studio 2' -c:v libx265 -an -crf 27 -preset ultrafast -tune zerolatency -bsf:v hevc_mp4toannexb -f mpegts 'srt://0.0.0.0:7000?pkt_size=1316&mode=listener&nakreport=1&listen_timeout=-1'; do
+  echo Stream disrupted, retrying in 3 seconds...
+  sleep 3
+done
