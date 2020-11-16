c4245967de612f2b5af3469c0a50e6f9a9bb72fe
hadrienmarquez <hadrienmarquez@gmail.com>
2020-11-16 17:04:26 +0100

    Adding flag support for source and passphrase option

--- ffmpeg/scripts/startStream.sh
+++ ffmpeg/scripts/startStream.sh
@@ -5,6 +5,18 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/lib64
 
 export PATH=$PATH:/usr/local/bin
 
+while getopts p:s: flag
+do 
+  case "${flag}" in
+    p)   PASSPHRASE=${OPTARG};;
+    s)       SOURCE=${OPTARG};;
+    esac
+done
+
+echo $SOURCE;
+echo $PASSPHRASE;
+
+
 # Check if ffmpeg running, if running kill it
 SERVICE="ffmpeg"
 if ps -C $SERVICE >/dev/null
@@ -14,9 +26,33 @@ then
     echo "$SERVICE is killed........."
 fi
 
-# Here goes the big command
-ffmpeg -sources decklink
-until ffmpeg -format_code Hp25 -f decklink -i 'DeckLink Studio 2' -c:v libx265 -an -crf 27 -preset ultrafast -tune zerolatency -bsf:v hevc_mp4toannexb -f mpegts 'srt://0.0.0.0:7000?pkt_size=1316&mode=listener&nakreport=1&listen_timeout=-1'; do
-  echo "Stream disrupted, retrying in 3 seconds..."
-  sleep 3
-done
+
+if [[ $SOURCE = decklink ]]
+then
+  ffmpeg -sources decklink
+  if [ -z "${PASSPHRASE}" ]
+  then
+    until ffmpeg -format_code Hp25 -f decklink -i 'DeckLink Studio 2' -c:v libx265 -an -crf 27 -preset ultrafast -tune zerolatency -bsf:v hevc_mp4toannexb -f mpegts 'srt://0.0.0.0:7000?pkt_size=1316&mode=listener&nakreport=1&listen_timeout=-1'; do
+      echo "Stream disrupted, retrying in 3 seconds..."
+      sleep 3
+    done
+  else
+    until ffmpeg -format_code Hp25 -f decklink -i 'DeckLink Studio 2' -c:v libx265 -an -crf 27 -preset ultrafast -tune zerolatency -bsf:v hevc_mp4toannexb -f mpegts "srt://0.0.0.0:7000?pkt_size=1316&mode=listener&nakreport=1&listen_timeout=-1&pbkeylen=16&passphrase=$PASSPHRASE"; do
+      echo "Stream disrupted, retrying in 3 seconds..."
+      sleep 3
+    done
+  fi
+else
+  if [ -z "${PASSPHRASE}" ] 
+  then
+    until ffmpeg -re -loop 1 -f image2pipe -i ./sources/$SOURCE -c:v libx265 -an -crf 27 -preset ultrafast -tune zerolatency -bsf:v hevc_mp4toannexb -f mpegts 'srt://0.0.0.0:7000?pkt_size=1316&mode=listener&nakreport=1&listen_timeout=-1'; do
+      echo "Stream disrupted, retrying in 3 seconds..."
+      sleep 3
+    done
+  else
+    until ffmpeg -re -loop 1 -f image2pipe -i ./sources/$SOURCE -c:v libx265 -an -crf 27 -preset ultrafast -tune zerolatency -bsf:v hevc_mp4toannexb -f mpegts "srt://0.0.0.0:7000?pkt_size=1316&mode=listener&nakreport=1&listen_timeout=-1&pbkeylen=16&passphrase=$PASSPHRASE"; do
+      echo "Stream disrupted, retrying in 3 seconds..."
+      sleep 3
+    done
+  fi
+fi
